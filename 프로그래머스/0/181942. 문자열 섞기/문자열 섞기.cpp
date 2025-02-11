6735c3272d29385ada7d12e2fbaef6300e6790b6
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2025-02-11 14:15:30 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cpp
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cpp
@@ -6,11 +6,13 @@ using namespace std;
 string solution(string str1, string str2) {
     string answer = "";
 
+    
     for(int i = 0; i < str1.length(); i++ ){
 
         answer += str1[i];
         answer += str2[i];
     
+        
     }
 
     return answer;
