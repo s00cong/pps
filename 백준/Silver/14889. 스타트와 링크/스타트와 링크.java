119d989edcff5d289a4aaf520df611f0cfe28887
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2025-10-02 05:10:52 +0900

    [Silver I] Title: 스타트와 링크, Time: 328 ms, Memory: 15196 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static int[][] map;
+	static boolean[] visit;
+	
+	static int min = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		map = new int[N][N];
+		visit = new boolean[N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		combi(0, 0);
+		System.out.println(min);
+
+	}
+
+	static void combi(int idx, int count) {
+		if(count == N / 2) {
+		
+			diff();
+			return;
+		}
+
+		for(int i = idx; i < N; i++) {
+			if(!visit[i]) {
+				visit[i] = true;	// 방문으로 변경
+				combi(i + 1, count + 1);	
+				visit[i] = false;	
+			}
+		}
+	}
+
+	static void diff() {
+		int team_start = 0;
+		int team_link = 0;
+
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (visit[i] == true && visit[j] == true) {
+					team_start += map[i][j];
+					team_start += map[j][i];
+				}
+				// i 번째 사람과 j 번째 사람
+				else if (visit[i] == false && visit[j] == false) {
+					team_link += map[i][j];
+					team_link += map[j][i];
+				}
+			}
+		}
+		int val = Math.abs(team_start - team_link);
+		
+		
+		if (val == 0) {
+			System.out.println(val);
+			System.exit(0);
+		}
+		
+		min = Math.min(val, min);
+				
+	}
+
+}
