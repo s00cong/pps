cc03f747521084afbce68ed7f54ca989675d5346
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2025-10-28 23:56:12 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,27 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int[] a, b;
-    static int ret = Integer.MAX_VALUE;    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        a = new int[n];
-        b = new int[n];
-        for(int i = 0; i < n; i++){
-            a[i] = sc.nextInt();
-            b[i] = sc.nextInt();
-        }
-        solve(0, 1, 0, false);
-        System.out.println(ret);
-    }
+    static int N;
+    static long min_sum = Long.MAX_VALUE; 
+    // 신맛은 곱, 쓴맛은 합
+    static int[] S;
+    static int[] B;
 
-    public static void solve(int now, int a_gob, int b_sum, boolean used){
-        if(now == n){
-            if(used) ret = Math.min(ret, Math.abs(a_gob - b_sum));
-            return;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st;
+        S = new int[N];
+        B = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            S[i] = Integer.parseInt(st.nextToken()); // 신맛
+            B[i] = Integer.parseInt(st.nextToken()); // 쓴맛
         }
-        solve(now + 1, a_gob * a[now], b_sum + b[now], true);
-        solve(now + 1, a_gob, b_sum, used);
+
+        // 공집합(0)은 제외, 전체집합 포함 -> mask: 1 .. (1<<N)-1
+        for (int mask = 1; mask < (1 << N); mask++) {
+            long sourProd = 1L; // 신맛 곱
+            long bitterSum = 0L; // 쓴맛 합
+
+            for (int i = 0; i < N; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    sourProd *= (long) S[i];
+                    bitterSum += (long) B[i];
+                }
+            }
+            long diff = Math.abs(sourProd - bitterSum);
+            if (diff < min_sum) min_sum = diff;
+        }
+
+        System.out.println(min_sum);
     }
 }
