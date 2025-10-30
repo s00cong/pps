d891ab3190c9421664e2a04bdc4817f34cbc65c3
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2025-10-31 02:46:16 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 56024 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,35 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
-    static int N,M;
+    static int N, M;
+    static boolean[] visited;
     static int[] numbers;
-    static int[] visited;
-    public static void main(String[] args) {    //순열
-        
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         N = sc.nextInt();
         M = sc.nextInt();
-        visited = new int[N+1];
+        visited = new boolean[N+1];
         numbers = new int[M];
-        dfs(0);
+        // 순열
+        Permutation(0);
+        System.out.println(sb.toString());
     }
 
-    private static void dfs(int cnt){
-
+    static void Permutation(int cnt){
         if(cnt == M){
-            for (int i = 0; i < M; i++) {
-                System.out.print(numbers[i] + " ");
+            for(int i = 0 ; i<M;i++){
+                sb.append(numbers[i]+" ");
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
 
         for(int i = 1; i<=N; i++){
-            if(visited[i]==1) continue;
-            visited[i] = 1;
+            if(visited[i]) continue;
             numbers[cnt] = i;
-            dfs(cnt+1);
-            visited[i] = 0;
+            visited[i] = true;
+            Permutation(cnt+1);
+            visited[i] = false;
         }
+
     }
 }
