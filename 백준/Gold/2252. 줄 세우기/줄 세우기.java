956b2b95a29142879acf793b0a0edbd049582e07
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2025-11-28 00:10:12 +0900

    [Gold III] Title: 줄 세우기, Time: 368 ms, Memory: 45476 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,53 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    static int N , M; // 정점, 간선
-    static ArrayList<Integer>[] list; 
-    static int[] inD; 
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        N = sc.nextInt();
-        M = sc.nextInt();
-        list = new ArrayList[N+1];
-        inD = new int[N+1];
-        for(int i = 1 ; i<N+1; i++){
-            list[i] = new ArrayList<>();
-        }
-        int from,to;
-        for(int i = 0; i<M;i++){
-            from  = sc.nextInt();
-            to = sc.nextInt();
-            list[from].add(to);
-            inD[to]++;
-        }
-        Queue<Integer> q = new ArrayDeque<>();
+
+    static int N,M;
+    static int[] indegree;
+    static List<Integer>[] g;
+    static Queue<Integer> q = new ArrayDeque<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        indegree = new int[N+1];
+        g = new ArrayList[N+1];
         for(int i = 1; i<=N; i++){
-            if (inD[i]==0) {
-                q.offer(i);
-            }
+            g[i] = new ArrayList<>();
         }
 
-        // 가지치기. 큐사이즈가 0이면 위상 정렬 불가 
+        for(int i = 0 ; i < M ; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            indegree[b]++;
+            g[a].add(b); // 그래프 만들었다!
+        }
+
+        for(int i = 1; i<=N; i++){
+            if(indegree[i]==0)q.add(i);
+        }
         int idx;
         while(!q.isEmpty()){
             idx = q.poll();
             sb.append(idx).append(" ");
-            for(int i :list[idx] ){
-                inD[i]--;
-                if(inD[i]==0) {
-                    q.offer(i);
+            for(int next : g[idx]){
+                indegree[next]--;
+                if(indegree[next]==0){
+                    q.offer(next);
                 }
             }
         }
-        //가지치기 2. res.size() == N이여야함. 
 
         System.out.println(sb.toString());
-
     }
+    
 }
