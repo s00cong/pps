375a070434d83eec973ea723357b53c00062995b
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2025-12-29 22:26:35 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,18 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i<N; i++){
+            sb.append("LoveisKoreaUniversity ");
+        }
+        System.out.println(sb);
+        
+    }
+
+}
