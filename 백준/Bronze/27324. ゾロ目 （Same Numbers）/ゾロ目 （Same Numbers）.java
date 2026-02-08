5a519dda0f30e2db4b80c5bae815b1d1c0a73e0f
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2026-02-08 23:02:46 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 60 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+
+        System.out.println((n / 10 == n % 10) ? 1 : 0);
+    }
+}
