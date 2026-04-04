7d3dea24c07c03f9a8c034687cd6f94692ba03cc
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2026-04-04 21:50:02 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,37 +1,37 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {    //조합
+public class Main {
     static int N,M;
-    static int[] numbers;
-    static StringBuilder sb= new StringBuilder();
+    static int[] arr;
+    static int[] visited;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        numbers = new int[N+1];
-        
-        Comb(0, 1);
-        System.out.println(sb.toString());
+        arr = new int[M];
+        visited = new int[N+1];
+
+        solve(0,1);
     }
 
-    static void Comb(int cnt, int start){
-        if(cnt == M){
-            for(int i = 0 ; i<M; i++){
-                sb.append(numbers[i]+" ");
-                
+    static void solve(int cnt, int start){
+        if(cnt == M) {
+            for(int i = 0; i<M;i++){
+                System.out.print(arr[i]+" ");
             }
-            sb.append('\n');
+            System.out.println();
+            return;
         }
-
-        for(int i = start; i<=N; i++){
-            numbers[cnt] = i;
-            Comb(cnt+1, i+1);
+        for(int i = start; i<=N; i++){            
+            arr[cnt]=i;
+            solve(cnt+1,i+1);
         }
-
     }
 }
