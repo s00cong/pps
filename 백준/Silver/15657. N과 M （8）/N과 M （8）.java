b6dcc1866439c51ed2d53939f9b600dcb57c60fd
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2026-04-04 23:02:36 +0900

    [Silver III] Title: N과 M (8), Time: 108 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,46 +1,47 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
+    static int N,M;
     static int[] arr;
-    static int[] result;
+    static int[] visited;
+    static int[] numbers;
     static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        
+        arr = new int[M];
+        visited = new int[N+1];
 
-        arr = new int[N];
-        result = new int[M];
-
+        numbers = new int[N+1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i = 1 ; i <= N; i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(arr);
-
-        dfs(0, 0);
-
-        System.out.print(sb);
+        Arrays.sort(numbers);
+        solve(0,1);
+        System.out.println(sb);
     }
 
-    static void dfs(int depth, int start) {
-        if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(result[i]).append(" ");
+    static void solve(int cnt, int start){
+        if(cnt == M) {
+            for(int i = 0; i<M;i++){
+                sb.append(arr[i]).append(" ");
             }
-            sb.append("\n");
+            sb.append('\n');
             return;
         }
-
-        for (int i = start; i < N; i++) {
-            result[depth] = arr[i];
-            dfs(depth + 1, i);   
+        for(int i = start; i<=N; i++){
+            arr[cnt]=numbers[i];
+            solve(cnt+1,i);
         }
     }
 }
