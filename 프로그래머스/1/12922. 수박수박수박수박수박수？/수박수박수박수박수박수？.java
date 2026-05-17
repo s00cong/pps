3ec2fe63f7e999cd26e6a9864d5c86beb8286674
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2026-05-17 17:28:26 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.49 ms, Memory: 59.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        StringBuilder sb= new StringBuilder();
+        for(int i = 1; i <= n; i++){
+            sb.append(i % 2 == 1 ? "수" : "박"); 
+        }
+        return sb.toString();
+    }
+}
