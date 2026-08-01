5f0bc1055a80a8a1271741af5695fd6b83a64cb4
숩니s00cong <90634773+s00cong@users.noreply.github.com>
2026-08-01 17:29:24 +0900

    [level 2] Title: 문자열 압축, Time: 3.02 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int solution(String s) {
+        int answer = s.length(); 
+        
+        for(int i = 1; i <= s.length()/2; i++){
+            StringBuilder sb = new StringBuilder();
+            String prev = s.substring(0,i);
+            
+            int cnt = 1;
+
+            for(int j = i; j < s.length(); j += i){
+                String cur = s.substring(j,Math.min(j+i,s.length()));
+
+                if(prev.equals(cur)){
+                    cnt++;
+                }
+                else{
+                    if(cnt>1){
+                        sb.append(cnt);
+                    }
+                    sb.append(prev);
+                    cnt = 1;
+                    prev = cur;
+                }
+
+            }
+            if(cnt>1) {
+                sb.append(cnt);
+            }
+            sb.append(prev);
+
+            answer = Math.min(answer,sb.length());
+
+        }
+        return answer;
+    }
+}
